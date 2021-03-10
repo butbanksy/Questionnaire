9039753a51f52a483c4c38622898cb0fe7424db5
Akashi <donsimoo97@gmail.com>
2021-03-10 11:32:11 +0100

    add services impl

---
+++ Questionnaire/Implementations/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Questionnaire.Interfaces.Services;
+using Questionnaire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questionnaire.Implementations.Services
+{
+    public class UserService : IUserService
+    {
+        public void AddAnswer(User user, Answer answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
