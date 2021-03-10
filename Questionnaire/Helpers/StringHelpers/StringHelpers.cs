f6320e365e850074bd9d6c2ea5f156403fbf08cd
El Mehdi El Allaoui <elallaoui.elmehdi@gmail.com>
2021-03-10 11:57:37 +0100

    initial program + IQuestionService

---
+++ Questionnaire/Helpers/StringHelpers/StringHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questionnaire.Helpers.StringHelpers
+{
+    public static class StringHelpers
+    {
+
+        public static bool isNullOrEmpty(this string s)
+        {
+
+            return s == null || s == "";
+        }
+
+        public static bool isValidMail(this string s)
+        {
+
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(s);
+                return addr.Address == s;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Questionnaire/Interfaces/Services/IQuestionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Questionnaire.Models;
+
+namespace Questionnaire.Interfaces.Services
+{
+    public interface IQuestionService
+    {
+        IEnumerable<Question> GetQuestions();
+    }
+}
