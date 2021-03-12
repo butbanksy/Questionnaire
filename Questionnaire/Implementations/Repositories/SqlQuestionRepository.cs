b37bbd74dfbd724eb64d2e25623d9f4f99b57cf8
butbanksy <mrhazzoul@gmail.com>
2021-03-12 11:34:46 +0100

    Added DbContext and injected services in QuestionnaireAPI

---
+++ Questionnaire/Implementations/Repositories/SqlQuestionRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Questionnaire.Data;
+using Questionnaire.Interfaces.Repositories;
+using Questionnaire.Models;
+
+namespace Questionnaire.Implementations.Repositories
+{
+    public class SqlQuestionRepository : IQuestionRepository
+    {
+        private readonly QuestionnaireContext _context;
+
+        public SqlQuestionRepository(QuestionnaireContext context)
+        {
+            _context = context;
+        }
+
+        public SqlQuestionRepository()
+        {
+        }
+        public IEnumerable<Question> GetQuestions()
+        {
+            return _context.Questions;
+        }
+
+        public Question GetQuestionById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
